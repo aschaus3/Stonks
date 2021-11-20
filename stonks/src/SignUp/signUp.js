6f3e1dba34f9ad35658e383d4b578214b80be4d1
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-20 12:35:26 -0500

    fsd

--- stonks/src/SignUp/signUp.js
+++ stonks/src/SignUp/signUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './signUp.css';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
@@ -6,26 +6,44 @@ import {useNavigate} from 'react-router-dom';
 import { render } from 'react-dom';
 
 
+class SignUp extends Component
+{
+  constructor() {
+    super();
+    this.usrName = React.createRef();
+    this.pass = React.createRef();
+    this.state = { user: {} };
+    this.onSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleSubmit(e) 
+  {
+    e.preventDefault();
+    fetch('http://localhost:5000/add-user', { 
+        method: 'POST',
+        body: {
+          username: this.usrName,
+          password: this.pass
+        }
+      })
+      .then(function(response) {
+        console.log(response.json());
+      }).then(function(body) {
+        console.log(body);
+      });
+  }
 
-
-
-function SignUp() {
+  render() {
     return (
-      <div className="signUp">
-        <div className="signUp-body">
-            <div> SignUp Here </div> 
-            <input placeholder={"Username"} className="username" type="text"/>
-            <input placeholder={"Password"} className="password" type="password" />
-            <input placeholder={"Confirm Password"} className="password" type="password" />
+      <form onSubmit={this.onSubmit}>
+        <input id="usr" type="text" placeholder="Username" ref={this.usrName}/>
+        <input type="text" placeholder="Password" ref={this.pass}/>
+        <input type="submit" />
+      </form>
+    );
+  }
 
-        </div>
-        
-
-      </div>
-      
-    )
-    
 }
-  
+
+
   export default SignUp;
