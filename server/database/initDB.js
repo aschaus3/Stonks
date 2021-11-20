e5e00aa8b2c90bc904a0105d8645b97bf830f59d
aadhieaswar <aadhieaswar@gmail.com>
2021-11-20 18:16:23 -0500

    fixed issue with authenticating user

--- server/database/initDB.js
+++ server/database/initDB.js
@@ -1,3 +1,4 @@
+const cryptoJS = require('crypto-js');
 const { createConnection, user, transactions }  = require('./DBVariables');
 
 // create the connection
@@ -5,6 +6,15 @@ const conn = createConnection();
 
 // connect to the database
 conn.connect();
+
+// drop the table if it exists
+conn.query(`
+    DROP TABLE IF EXISTS ${transactions};
+  `, (err) => {
+    if (err) throw (err);
+
+    console.log(`Successfully Dropped ${transactions} Table!`);
+});
 
 // drop the table if it exists
 conn.query(`
@@ -29,13 +39,17 @@ conn.query(`
   console.log(`Successfully Created ${user} Table!`);
 });
 
-// drop the table if it exists
-conn.query(`
-    DROP TABLE IF EXISTS ${transactions};
-  `, (err) => {
-    if (err) throw (err);
+// encrypt password
+let password = '123';
+password = cryptoJS.AES.encrypt(JSON.stringify({ password }), 'secret').toString();
 
-    console.log(`Successfully Dropped ${transactions} Table!`);
+// Insert Sample User for Testing purposes
+conn.query(`
+  INSERT INTO ${user} VALUES (NULL, "Jon", \"${password}\");
+`, (err) => {
+  if (err) throw err;
+
+  console.log('Successfully Added Sample User!');
 });
 
 // create the table for users
@@ -43,7 +57,7 @@ conn.query(`
   CREATE TABLE ${transactions} (
     TransactionID INT AUTO_INCREMENT
     ,Coin VARCHAR(50) NOT NULL
-    ,UserID VARCHAR(50) NOT NULL
+    ,UserID INT NOT NULL
     ,IsSelling BOOLEAN
     ,IsBuying BOOLEAN
     ,PRIMARY KEY (TransactionID)
