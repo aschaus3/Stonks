d41a23a4e33fcc646e162ab724740ee7836f7ee0
jadifalco <jadifalco@gmail.com>
2021-11-20 00:32:15 -0500

    login

--- stonks/src/Login/login.js
+++ stonks/src/Login/login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './login.css'
 
 function Login() {
     return (
-      <div className="App">
-        <div className="App-body">
+      <div className="login">
+        <div className="login-body">
             <div> Login </div> 
+            <input placeholder={"Username"} className="username" type="text"/>
+            <input placeholder={"Password"} className="password" type="password" />
+            <button className="login-button"> Login </button>
+            <div>
+                Don't have an account,
+                <button className="login-button"> Sign Up </button>
+            </div>
         </div>
       </div>
     );
