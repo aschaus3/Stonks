928bca5827200889d7578e4a98d8968a393c7b55
Anuj Kulkarni <anujkulkarni@Anujs-MacBook-Pro.local>
2021-11-19 21:39:37 -0500

    header and footer push

---
+++ stonks/src/Footer.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import './footer.css'
+
+function Footer()    {
+    return(
+        <div>
+            Hello
+        </div>
+    );
+}
+
+
+export default Footer;
---
+++ stonks/src/Header.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import './header.css'
+
+function Header()    {
+    return(
+        <div>
+            Hello
+        </div>
+    );
+}
+
+
+export default Header;
