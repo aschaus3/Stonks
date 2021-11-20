71fd01a50a219e84066de1504c560075df419b6d
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-19 22:42:58 -0500

    f

--- stonks/src/Footer.js
+++ stonks/src/Footer.js
@@ -4,12 +4,12 @@ import './footer.css'
 
 function Footer()    {
     return(
-            <div className={ "header-container" }>
-          <img src={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/challenge_thumbnails/000/723/506/datas/original.png"} className={"img-icon"} />
-          <h4>
-            Stonks
-          </h4>
-          <button placeholder={"Help"} className="help-button" />
+          <div className={ "header-container" }>
+            <img src={"https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/challenge_thumbnails/000/723/506/datas/original.png"} className={"img-icon"} />
+            <h4>
+              Stonks
+            </h4>
+            <button placeholder={"Help"} className="help-button" />
         </div>
     );
 }
