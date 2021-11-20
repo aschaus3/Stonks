7a2ca4a82e6831c09ad63620c18fb3e3a43d4a83
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-20 00:09:43 -0500

    login beginnig

--- stonks/src/Login/login.js
+++ stonks/src/Login/login.js
@@ -4,15 +4,7 @@ function Login() {
     return (
       <div className="App">
         <div className="App-body">
-          <div className="stonk-container">
-            <div className="stonk">
-                <h1>Fav Stonks</h1>
-            </div>
-  
-            <div className="stonk">
-                <h1>Top Stonks</h1>
-            </div>
-          </div>
+            <div> Login </div> 
         </div>
       </div>
     );
