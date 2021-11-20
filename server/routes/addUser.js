be1f9bc15238c85ffb1a80c0e34297a7cd809125
aadhieaswar <aadhieaswar@gmail.com>
2021-11-20 01:48:50 -0500

    added authentication and create user routes

---
+++ server/routes/addUser.js
@@ -0,0 +1,35 @@
+// import required packages and modules
+const express = require('express');
+const cryptoJS = require('crypto-js');
+const { getAllUsers, addUser } = require('../database/asyncQueries');
+
+// use the express router
+const router = express.Router();
+
+// body parser to be able to read post data
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.post('/', (req, res) => {
+  const username = req.body.username;
+  let password = req.body.password;
+  password = cryptoJS.AES.encrypt(JSON.stringify({ password }), 'secret').toString();
+
+  getAllUsers().then((users) => {
+    for (let user of users) {
+      if (username === user.Username) {
+        res.send({ errMsg: 'Username Already Exists' });
+        return;
+      }
+    }
+
+    addUser(username, password).then(() => {
+      res.send({ msg: 'User Added Successfully!' });
+      return;
+    }).catch((err) => { console.log(err) });
+
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+module.exports = router;
