cdb90de8a8c01e4ebf3f44b8fffc5dd68dcef17f
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-20 18:40:02 -0500

    drop down

--- stonks/src/Api/Coin.js
+++ stonks/src/Api/Coin.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import { Collapse } from 'react-bootstrap';
 import './coin.css';
 import { Link } from 'react-router-dom';
-import { FaChevronCircleDown} from 'react-icons/fa';
+import { FaChevronDown } from 'react-icons/fa';
+import Expand from 'react-expand-animated';
 
 const Coin = ({image, name, symbol, price, volume, priceChange, marketCap}) => {
-  let open = false;
+  
+  const [clicked, setClicked] = useState(false);
 
-  let toggleClick = () => {
-    open = !open;
-  }
     //UI element
     return (
         <div className="coinContainer" >
             <div className="coinRow" for="expend">
                 <div className="coin" for="extend">
-                    <img src={image} alt="crypto" onClick={toggleClick}></img>
+                    <img src={image} alt="crypto"></img>
                     <h1>{name}</h1>
                     <h1 className="coinSymbol">{symbol}</h1>
                     <h1 className="coinPrice">${price}</h1>
                     <div className="buy-button">
-
-                    {priceChange < 0 ? (
-                    <h1 className="red" >{priceChange.toFixed(2)}%</h1>
-                    ) : (
-                    <h1 className="green" >{priceChange.toFixed(2)}%</h1>
-                    )}
-                    {/*<Link to="/Trade" state={{from:name}}className="buy-button" >
+                    <Link to="/Trade" state={{from:name}}className="buy-button" >
                         Buy
                     </Link>
-                    */}
                     </div>
-                    <div className="iconDiv">
-                        <FaChevronCircleDown className="icon"/>
-                    </div> 
+                    <i >
+                        <FaChevronDown className="icon" onClick={() => setClicked(!clicked)}/>
+                    </i> 
+
                 </div>
+                <Expand open={clicked}>
+                        <div style={{ width: '300px', height: '400px', color: 'red' }}>Hello</div>
+                    </Expand>
                 {/*
                 <div className="coinData">
                     
