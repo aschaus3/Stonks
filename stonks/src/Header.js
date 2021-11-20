49ea2350b2eb198aab502ea462e92d042447e584
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-19 23:09:27 -0500

    Aligned the middle texts in header and footer

--- stonks/src/Header.js
+++ stonks/src/Header.js
@@ -6,7 +6,7 @@ function Header()    {
     return(
         <div className={ "header-container" }>
           <img src={"https://pbs.twimg.com/profile_images/1280883056979726339/9YesHxc2_400x400.jpg"} className={"img-icon"} />
-          <h4>
+          <h4 id="header-middle">
           Stonks
           </h4>
         <div>
