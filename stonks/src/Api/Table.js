230b68ff2eaa61b2fcbc13e0c41a416bbcfd111d
aadhieaswar <aadhieaswar@gmail.com>
2021-11-21 01:27:15 -0500

    fixed display of the watch list component

--- stonks/src/Api/Table.js
+++ stonks/src/Api/Table.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Coin from './Coin'
 import './table.css'
 import ListBoughtCoin from './ListBoughtCoin';
 import AddCoin from './AddCoin';
 import axios from 'axios';
+import { WatchList } from './context/WatchList';
 
 //https://api.coingecko.com/api/v3/coins/markets?vs_currency=CAD&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
@@ -15,6 +16,9 @@ function Table()    {
     //initializing use state
     const [ search, setSearch ] = useState(' ')
 
+    // get watchList
+    const { watchList } = useContext(WatchList);
+
     //creating axios connection
     useEffect(() => {
         axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=CAD&order=market_cap_desc&per_page=500&page=1&sparkline=false')
@@ -22,7 +26,7 @@ function Table()    {
                 setCoins(res.data)
             }).catch(err => console.log(err))
     }, []);
-     
+
     //handling change in input
     const handleChange = e =>   {
         setSearch(e.target.value);
@@ -37,28 +41,30 @@ function Table()    {
     return(
         <div>
             <div className="boughtCoinApp">
-                <AddCoin/>
-                <ListBoughtCoin/>
+            <AddCoin/>
+            { (watchList.length > 0) &&  <><ListBoughtCoin/></> }
+
+
             </div>
-        
+
             <div className="coinApp">
                 <div className="coinSearch">
                     <h1 className="coinText">Search a Currency</h1>
-                    <form className="searchForm"> 
-                        <input type="text" placeholder="Search" 
+                    <form className="searchForm">
+                        <input type="text" placeholder="Search"
                                 className="coinInput" onChange={handleChange}/>
                     </form>
                 </div>
                 {filteredCoins.map(coin =>  {
                     return(
-                        <Coin key={coin.id} name={coin.name} 
+                        <Coin key={coin.id} name={coin.name}
                         price={coin.current_price} image={coin.image}
                         symbol={coin.symbol} marketCap={coin.market_cap}
                         priceChange={coin.price_change_percentage_24h}
                         volume={coin.total_volume}/>
                     );
                 })}
-            
+
             </div>
         </div>
     );
